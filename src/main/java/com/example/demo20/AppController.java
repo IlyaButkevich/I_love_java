c7bae5ff1ff073f2cb6263bab72ba70a29a57327
ilya <i.v.butkev@gmail.com>
2024-10-21 18:15:54 +0300

    small improvements

--- src/main/java/com/example/demo20/AppController.java
+++ src/main/java/com/example/demo20/AppController.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.Collections;
 import java.text.SimpleDateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param; //привязка пераметров
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +18,67 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Controller
-public class GoodsController {
+public class AppController {
 
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping("/") // Главная страница
+    @RequestMapping("/")
     public String index(Model model, @Param("keyword") String keyword) {
+        List<Goods> goodsList = goodsService.ListAll(keyword);
+
+        model.addAttribute("goodsList", goodsList);
+        model.addAttribute("keyword", keyword);
+        return "index";
+    }
+
+
+    boolean sorted = true;
+    @RequestMapping("sort")
+    public  String sortHomePage(Model model, @Param("keyword") String keyword){
+        List<Goods> goodsList = goodsService.ListAll(keyword);
+        goodsList.sort(Comparator.comparing(Goods::getDateofarrival));
+
+        if (sorted==false){
+            goodsList.sort(Comparator.comparing(Goods::getDateofarrival).reversed());
+        }
+
+        model.addAttribute("goodsList", goodsList);
+        model.addAttribute("keyword", keyword);
+
+        sorted = !sorted;
+        return "index";
+    }
+
+    @RequestMapping("/new")
+    public String newGoods(Model model){
+        Goods goods = new Goods();
+        model.addAttribute("goods", goods);
+        return "new";
+    }
+
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public String saveGoods(@ModelAttribute("goods") Goods goods){
+        goodsService.save(goods);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView editGoods(@PathVariable(name="id") Long id){
+        ModelAndView mav = new ModelAndView("edit_good");
+        Goods goods = goodsService.get(id);
+        mav.addObject("goods", goods);
+        return mav;
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteGoods(@PathVariable Long id){
+        goodsService.delete(id);
+        return "redirect:/";
+    }
+
+    @RequestMapping("getgraph")
+    public  String showGraph(Model model, @Param("keyword") String keyword){
         List<Goods> goodsList = goodsService.ListAll(keyword);
 
         Map<Date, Integer> dateMap = new HashMap<>();
@@ -57,78 +111,9 @@ public class GoodsController {
         });
 
         model.addAttribute("chartData", dateCountMap);
-        model.addAttribute("goodsList", goodsList);
-        model.addAttribute("keyword", keyword);
-        return "index"; // index.html
+
+        return "graph";
     }
-
-    @RequestMapping("/new")
-    public String newGoods(Model model){
-        Goods goods = new Goods();
-        model.addAttribute("goods", goods);
-        return "new";
-    }
-
-    @RequestMapping(value="/save", method = RequestMethod.POST)
-    public String saveGoods(@ModelAttribute("goods") Goods goods){
-        goodsService.save(goods);
-        return "redirect:/";
-    }
-
-    @RequestMapping("/edit/{id}")
-    public ModelAndView editGoods(@PathVariable(name="id") Long id){
-        ModelAndView mav = new ModelAndView("edit_good");
-        Goods goods = goodsService.get(id);
-        mav.addObject("goods", goods);
-        return mav;
-    }
-
-    @RequestMapping("/delete/{id}")
-    public String deleteGoods(@PathVariable Long id){
-        goodsService.delete(id);
-        return "redirect:/";
-    }
-
-    @RequestMapping("sort")
-    public  String sortHomePage(Model model, @Param("keyword") String keyword){
-        List<Goods> goodsList = goodsService.ListAll(keyword);
-        goodsList.sort(Comparator.comparing(Goods::getDateofarrival));
-
-        Map<Date, Integer> dateMap = new HashMap<>();
-
-        for (Goods good : goodsList) {
-            Date dateGoods = good.getDateofarrival();
-            dateMap.put(dateGoods, dateMap.getOrDefault(dateGoods, 0) + 1);
-        }
-
-        List<List<Object>> dateCountMap = new ArrayList<>();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-        for (Map.Entry<Date, Integer> entry : dateMap.entrySet()) {
-            List<Object> subList = new ArrayList<>();
-            subList.add(sdf.format(entry.getKey()));
-            subList.add(entry.getValue());
-            dateCountMap.add(subList);
-        }
-
-        Collections.sort(dateCountMap, new Comparator<List<Object>>() {
-            public int compare(List<Object> o1, List<Object> o2) {
-                try {
-                    Date date1 = sdf.parse((String) o1.get(0));
-                    Date date2 = sdf.parse((String) o2.get(0));
-                    return date1.compareTo(date2);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        model.addAttribute("chartData", dateCountMap);
-        model.addAttribute("goodsList", goodsList);
-        model.addAttribute("keyword", keyword);
-        return "index";
-    }
-
 
     @Autowired
     private MyAppUserRepository myAppUserRepository;
@@ -151,6 +136,16 @@ public class GoodsController {
     @GetMapping("/req/signup")
     public String signup(){
         return "signup";
+    }
+
+    @RequestMapping("goback")
+    public String goBack(){
+        return "redirect:/";
+    }
+
+    @RequestMapping("tologout")
+    public String toLogout(){
+        return "login";
     }
 
 }
